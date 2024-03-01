454b8f90531efbd3807013b09d50acd56541e0f7
hieu1911 <doanvanhieu1911@gmail.com>
2024-03-01 17:20:27 +0700

    Window service

---
+++ WindowService/WindowService/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace WindowService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer timer = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer();
+
+            timer.Interval = 60000;
+
+            timer.Elapsed += TimerTick;
+
+            timer.Enabled = true;
+
+            Utilities.WriteLogError("Test for fisrt run Window service!!!");
+        }
+
+        private void TimerTick(Object sender, ElapsedEventArgs args)
+        {
+            Utilities.WriteLogError("Timer has ticked!!!");
+        }
+
+        protected override void OnStop()
+        {
+            timer.Enabled = true;
+            Utilities.WriteLogError("Window serivce has been stop!!!")
+        }
+    }
+}
---
+++ WindowService/WindowService/Utilities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowService
+{
+    public class Utilities
+    {
+        public static void WriteLogError(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTimeOffset.Now.ToString("dd/MM/yyy") + ": " + ex.Source + "; " + ex.Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public static void WriteLogError(string message) 
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTimeOffset.Now.ToString("dd/MM/yyy") + ": " + message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
